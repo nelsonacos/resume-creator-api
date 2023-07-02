eeb9a69edb50650f224fa58b9a642e375dc80559
Nelson Acosta <nelson.acosta.web@gmail.com>
2023-07-02 15:19:33 -0400

    docs: Improve the order in which routes are displayed in the root api

--- resume/urls.py
+++ resume/urls.py
@@ -6,13 +6,14 @@ from rest_framework import routers
 from .views import LanguageViewSet, EducationInformationViewSet, WorkExperienceViewSet, ProfileDescriptionViewSet, ContactDataViewSet, PersonalDataViewSet, ProfileViewSet
 
 router = routers.DefaultRouter()
-router.register(r'languages', LanguageViewSet)
-router.register(r'education-information', EducationInformationViewSet)
-router.register(r'work-experiences', WorkExperienceViewSet)
-router.register(r'profile-descriptions', ProfileDescriptionViewSet)
-router.register(r'contact-data', ContactDataViewSet)
-router.register(r'personal-data', PersonalDataViewSet)
 router.register(r'profiles', ProfileViewSet)
+router.register(r'personal-data', PersonalDataViewSet)
+router.register(r'contact-data', ContactDataViewSet)
+router.register(r'profile-descriptions', ProfileDescriptionViewSet)
+router.register(r'work-experiences', WorkExperienceViewSet)
+router.register(r'education-information', EducationInformationViewSet)
+router.register(r'languages', LanguageViewSet)
+
 
 urlpatterns = [
     path('api/v1/', include(router.urls)),
