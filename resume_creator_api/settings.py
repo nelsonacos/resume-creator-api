45f1d260ba851a833f47aa1b86a7d987c7aa3ee2
Nelson Acosta <nelson.acosta.web@gmail.com>
2023-07-02 15:17:33 -0400

    Add changes to successfully serve static files

--- resume_creator_api/settings.py
+++ resume_creator_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8ps&h4snq(-)$$@f-3vhp&dk7)hf@dnd19(4q_a+im^926hxu_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
 
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
