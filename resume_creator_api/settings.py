ccd09a810f1ea3135003587c8b1657b860095553
Nelson Acosta <nelson.acosta.web@gmail.com>
2023-07-05 10:14:04 -0400

    settings: Add minimum security measures in settings file

--- resume_creator_api/settings.py
+++ resume_creator_api/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+import dotenv
+
+dotenv.load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-8ps&h4snq(-)$$@f-3vhp&dk7)hf@dnd19(4q_a+im^926hxu_"
+# SECRET_KEY = "django-insecure-8ps&h4snq(-)$$@f-3vhp&dk7)hf@dnd19(4q_a+im^926hxu_"
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KEY",
+    "django-insecure-8ps&h4snq(-)$$@f-3vhp&dk7)hf@dnd19(4q_a+im^926hxu_",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool(os.environ.get("DJANGO_DEBUG", True))
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "[::1]"]
 
