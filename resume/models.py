6ceeaee29670957b71da00aa4a0dce054a1c474b
Nelson Acosta <nelson.acosta.web@gmail.com>
2023-09-26 01:43:09 -0300

    fix: Remove required field  from model

--- resume/models.py
+++ resume/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 class Profile(models.Model):
-    name = models.CharField(max_length=100)
     photo = models.ImageField(upload_to="profile_photos/", blank=True)
     title = models.CharField(max_length=100)
 
